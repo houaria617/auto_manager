65e42e5164a3e053a15f5fd2a7bb5ef59d3a24ab
MISSOUNI Nacer Eddine <nacereddine.missouni@ensia.edu.dz>
2025-11-01 13:11:29 +0100

    Revert "feat:adding the screens for rantals,rental details and report and ana…"

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,20 @@
-import 'package:auto_manager/features/analytics/presentation/analytics.dart';
-import 'package:auto_manager/features/rentals/presentation/rental_details.dart';
-import 'package:auto_manager/features/rentals/presentation/rentals.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Rental App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white, // White AppBar background
-          foregroundColor: Colors.black, // Black icons/text
-          elevation: 0, // No shadow
-        ),
-        cardTheme: CardThemeData(
-          color: Colors.white, // White card background
-          elevation: 0, // No shadow
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
-            side: BorderSide(
-              color: Colors.grey.shade200,
-              width: 1,
-            ), // Light border
-          ),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
       ),
-      home: const RentalDetailsScreen(),
     );
   }
 }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Reports & Analytics',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: RentalsScreen(),
-//     );
-//   }
-// }
