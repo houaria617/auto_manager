a54881844bbf3c4117176a21f12cdfd6f8b83bfb
Yasmine <yasmine.meriche@ensia.edu.dz>
2025-12-06 23:46:45 +0100

    updated yaml

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
-import path_provider_foundation
-import printing
-import sqflite_darwin
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
-=======
 import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
->>>>>>> feat/auth-local
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
